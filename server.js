1f94ced5048bda3467ceb6197352649b76136c68
urie-w <urieasia@gmail.com>
2024-08-06 14:07:50 -0600

    New Repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./html-routes');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Run in this order
+app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+app.listen(PORT, () => {
+
+  //Routes
+  app.use('/', htmlRoutes);
+  app.listen(PORT, () => {
+    console.log(`Server now on port http;//localhost:${PORT}!`);
+  });
+});
