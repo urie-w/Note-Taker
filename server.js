b5792366fa5cceb1b2c9c29622f7d5140289c3b0
urie-w <urieasia@gmail.com>
2024-08-06 14:19:06 -0600

    Revising server.js

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express();
 // Run in this order
 app.use(urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, 'public')))
 
   //Routes
   app.use('/', htmlRoutes);
+
+
   app.listen(PORT, () => {
     console.log(`Server now on port http;//localhost:${PORT}!`);
   });
-});
